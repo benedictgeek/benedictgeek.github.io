a6ee05bbbb23ee70fd3daa2e482e14c67a6968b5
Olushola <olushola251@gmail.com>
2023-04-02 12:15:09 +0100

    added GA4 tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           name="twitter:description"
           content="Experienced software engineer, with a wide range of skillset, I enjoy building and contributing to products that improve people's lives."
         />
-         <meta
+        <meta
           name="og:description"
           content="Experienced software engineer, with a wide range of skillset, I enjoy building and contributing to products that improve people's lives."
         />
@@ -31,6 +31,16 @@ export default function Home() {
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBTK7V6');`,
+          }}
+        ></script>
       </Head>
       <AppContextProvider>
         <Header />
